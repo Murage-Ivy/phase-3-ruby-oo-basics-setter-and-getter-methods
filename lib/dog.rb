13126f1533561a56f3584f3539bf07949f291b97
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-10-18 12:38:10 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
-class Dog
+require "pry"
 
+class Dog
+  # setter for the name of the dog
+  def name=(name)
+    @name = name
+  end
+
+  # setter for the breed  of the dog
+  def breed=(breed)
+    @breed = breed
+  end
+
+  # getter for the name of the dog
+  def name
+    @name
+  end
+
+  # getter for the name of the dog
+  def breed
+    @breed
+  end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
-class Person
+require "pry"
 
+class Person
+  # gets the name of the person and the job
+  attr_reader :name, :job
+  
+  #sets the person's name(instance variable) to the name passed
+  def name=(name)
+    @name = name
+  end
+
+  # sets the person's job title(instance variable) to the value  passed
+  def job=(job)
+    @job = job
+  end
 end
+
+
